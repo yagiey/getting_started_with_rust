6eaa64cb82276830ec75ce451b0423ba743884c7
Yagiey <yagiey@gmail.com>
2024-02-11 16:32:15 +0900

    chapter 1

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+    example_01_04();
+    example_01_05();
+    example_01_06();
+    example_01_07();
+}
+
+fn example_01_04() {
+    print!("{}, {}!", "Hello", "world");
+}
+
+fn example_01_05() {
+    print!("First line\nSecond line\nThird line\n");
+}
+
+fn example_01_06() {
+    println!("text of the line");
+}
+fn example_01_07() {
+    print!("text of the line\n");
+}
